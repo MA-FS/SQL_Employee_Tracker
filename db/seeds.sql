f406d2ab091bc10b164401b50656eea91e5b2404
MA-FS <87353040+MA-FS@users.noreply.github.com>
2021-11-10 15:14:32 +0800

    Updated db with schema and seeds sql files. Table relationships created

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 4),
+       ("Salesperson", 80000, 4),
+       ("Lead Engineer", 145000, 1),
+       ("Software Engineer", 120000, 1),
+       ("Account Manager", 175000, 2),
+       ("Accountant", 120000, 2),
+       ("Lawyer", 200000, 3),
+       ("Legal Team Lead", 250000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, NULL),
+       ("Mike", "Chan", 2, 1),
+       ("Ashley", "Rodriguez", 3, NULL),
+       ("Kevin", "Tupik", 4, 3),
+       ("Kunal", "Singh", 5, NULL),
+       ("Malia", "Brown", 6, 5),
+       ("Sarah", "Lourd", 8, NULL),
+       ("Tom", "Allen", 7, 7);
+
